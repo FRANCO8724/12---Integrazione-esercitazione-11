49b8f652b3312993eefb7e823e62d7f67e8b4953
FRANCOHAILIMITIDELLEVOLUTO <121171151+FRANCO8724@users.noreply.github.com>
2024-03-05 00:25:17 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using _12___Integrazione_esercitazione_11;
+using System;
+using System.Runtime.Intrinsics;
+
+class Program
+{
+    public static void Main(string[] args, Scontrino scontrino, Scontrino scontrino)
+    {
+        
+
+        int scelta = 0;
+        Scontrino miaIstanza = new Scontrino();
+
+        //stuttura menù
+        do
+        {
+            //stampa delle opzioni
+
+            Console.Clear();
+            Console.WriteLine("12 - Integrazione esercitazione 11");
+            Console.WriteLine(" ");
+            Console.WriteLine("          1 - Aggiunta: ");
+            Console.WriteLine("          2 - Totale: ");
+            Console.WriteLine("          3 - Ordina: ");
+            Console.WriteLine("          4 - Ricerca: ");
+            Console.WriteLine("          5 - Calcolo: ");
+            Console.WriteLine(" ");
+            Console.WriteLine("0 - uscita");
+            Console.WriteLine(" ");
+            //scelta dell'opzione
+            Console.WriteLine("inserisci la scelta: ");
+            scelta = int.Parse(Console.ReadLine());
+
+
+            //esecuzione dell'opzione
+            switch (scelta)
+            {
+                case 1:
+  
+
+                    Console.WriteLine("Inserisci nome podotto: ");
+                    string a = Console.ReadLine();
+
+                    Console.WriteLine("Inserisci prezzo prodotto: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    miaIstanza.Aggiunta(a, b);
+
+                    break;
+
+                    case 2 :
+
+                    decimal tot = miaIstanza.Totale();
+                    Console.WriteLine("Il totale è: " + tot + " $");
+
+                    break;
+
+                case 3:
+
+                    
+
+                    break;
+
+                case 4:
+
+                    Console.WriteLine("Inserisci prodotto da cercare: ");
+                    string cerc = Console.ReadLine();
+                    string cercato = miaIstanza.Cerca(cerc);
+                    Console.WriteLine(cercato);
+
+                    break;
+
+                case 5:
+
+                    int y = 0;
+
+                    for(int i=0;i<100;i++)
+                    {
+
+                        Console.WriteLine("Inserisci le quantità di ciascun prodotto:: ");
+                        int l = Convert.ToInt32(Console.ReadLine());
+
+                        int camb = miaIstanza.Quantità(l,y);
+
+                    }
+
+                    break;
+
+                   
+
+
+
+            }
+            Console.ReadLine();
+        } while (scelta != 0);
+
+
+    }
+
+}
+}
---
+++ Scontrino.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12___Integrazione_esercitazione_11
+{
+    internal class Scontrino
+    {
+        private string[] articoli;
+        private int prezzo;
+        private string nome;
+        private int i = 0;
+
+        public Scontrino()
+        {
+            articoli = new string[100];
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public void Aggiunta(string nome,int prezzo)
+        {
+            articoli[i] = nome + ";" + Convert.ToString(prezzo);
+        }
+
+        public int Totale()
+        {
+            int tot = 0;
+
+            for(int E = 0;E<articoli.Length;E++)
+            {
+                if (articoli[E] != null)
+                {
+                    string[] prova = articoli[E].Split();
+                    tot = tot + Convert.ToInt32(prova[1]);
+                }  
+            }
+
+
+            return tot;
+        }
+
+        public void Ordina()
+        {
+          
+        }
+
+        public string Cerca(string nome)
+        {
+            int p = 0;
+
+            for(int E = 0; E<articoli.Length;E++)
+            {
+                if (articoli[E] != null)
+                {
+                    string[] prova = articoli[E].Split();
+
+                    if (prova[0] == nome)
+                    {
+                        p = 1;
+
+                        return "elemento trovato esso è : " + prova[0] + " prezzo:" + Convert.ToString(prova[1]);
+
+                        break;
+                    }
+                }
+            }
+                return "elemento non trovato";
+        }
+
+        public void Quantità(int a ,int b)
+        {
+            articoli[b] = articoli[b] + ";" + a;
+        }
+    }
+}
